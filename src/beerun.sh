930dc7d2967dd293a4badf58c04edb0a0a0bb5b0
Benjamin Aschenbrenner <benjamin.aschenbrenner@gmail.com>
2015-09-14 19:45:19 +0200

    added watchdog to restart beereader service on hangups and delayed the database dump startup

--- src/beerun.sh
+++ src/beerun.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-while [[ TRUE ]]; do
-	hcitool inq |grep -o '..:..:..:..:..:..' | tr '\n' '\0' | xargs -0 /home/benni/project/beehive-sensing/src/beehive_reader
-done
+hcitool inq |grep -o '..:..:..:..:..:..' | tr '\n' '\0' | xargs -0 /home/benni/project/beehive-sensing/src/beehive_reader
 
